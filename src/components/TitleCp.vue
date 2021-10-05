3cd907673b8ef61d8941b82a2978910290b8b0ee
woojungyang <blossom1113@nate.com>
2021-10-05 16:01:30 +0900

    first commit

---
+++ src/components/TitleCp.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header-wrapper">
+    <h1 class="logo-wrap"></h1>
+    <img src="@/assets/img/logo/png" alt="logo" class="logo">
+    {{mainTitle}}
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TitleCp',
+  props: ['title'],
+};
+</script>
+
+<style lang="scss" scoped>
+.header-wrapper{
+  padding:$gutter-wdith;
+  background-color: $secondary-color;
+  color:$primary-color;
+  font-size:1.5em;
+  .logo{
+    width:1.5em
+  }
+}
+
+</style>
